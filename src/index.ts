665289057473d0c5460a827a02d75e888cc42db3
Brady Caspar <bradycaspar@Bradys-MacBook-Pro.local>
2020-01-16 21:01:59 -0800

    adding a comment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as Component1 } from './component1';
 export { default as Component2 } from './component2';
-export { default as Component3 } from './component3';
+export { default as Component3 } from './component3'; //commenting this will cause the test to pass
